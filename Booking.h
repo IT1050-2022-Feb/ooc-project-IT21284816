424ead68b1f8ce897647bc2e9b57568352f885b5
IT21288326 <99243796+IT21288326@users.noreply.github.com>
2022-05-20 22:52:02 +0530

    Create Booking.h

---
+++ Booking.h
@@ -0,0 +1,18 @@
+//it21288326
+//U D S V Dharmasena
+
+#include "Booking.h"
+
+class Payment
+{
+private:
+	int PaymentID;
+	double fees;
+
+public:
+	Payment() {};
+	Payment(int pId, double fees);
+	void Display();
+	~Payment();
+
+};
